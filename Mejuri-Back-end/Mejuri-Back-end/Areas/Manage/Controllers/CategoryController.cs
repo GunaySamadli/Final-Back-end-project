1575704eb86c7eeec68694f47b8c72683c248ad0
GunaySamadli <gunayfs@code.edu.az>
2021-12-05 16:53:43 +0400

    Added Helper folder

---
+++ Mejuri-Back-end/Mejuri-Back-end/Areas/Manage/Controllers/CategoryController.cs
@@ -0,0 +1,29 @@
+﻿using Mejuri_Back_end.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mejuri_Back_end.Areas.Manage.Controllers
+{
+    [Area("manage")]
+    public class CategoryController : Controller
+    {
+        public readonly AppDbContext _context;
+        public CategoryController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Category> categories = _context.Categories.ToList();
+            return View(categories);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ Mejuri-Back-end/Mejuri-Back-end/Areas/Manage/Controllers/GenderController.cs
@@ -0,0 +1,25 @@
+﻿using Mejuri_Back_end.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mejuri_Back_end.Areas.Manage.Controllers
+{
+    [Area("manage")]
+    public class GenderController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public GenderController(AppDbContext context)
+        {
+           _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Gender> genders = _context.Genders.ToList();
+            return View(genders);
+        }
+    }
+}
